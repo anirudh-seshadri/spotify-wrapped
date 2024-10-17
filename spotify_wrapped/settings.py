f394ee7bddcf031b5e91d4f1e7ae13138297b0a1
Ani Seshadri <anirudhr2005@gmail.com>
2024-10-17 14:36:18 -0400

    User auth using spotify log in

--- spotify_wrapped/settings.py
+++ spotify_wrapped/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
