ea3a2eb0963f3db6b15726f9d6cd38a45bf525ec
Dhruv Sureka <dhruvsureka308@gmail.com>
2024-10-24 15:11:35 -0400

    refined the login page UI

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',       // Django template path for HTML files
+    './web_app/static/**/*.js',    // Updated path for JS files inside web_app/static
+    './web_app/static/**/*.css',   // Updated path for CSS files inside web_app/static
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
