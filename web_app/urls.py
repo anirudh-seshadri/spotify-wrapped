f7f29359bb0d3f870512d6a2dab6cbf4429706fb
Dhruv Sureka <dhruvsureka308@gmail.com>
2024-10-31 12:59:46 -0400

    Revised welcome and login page and addded transitions

--- web_app/urls.py
+++ web_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('login/', views.login, name='login'),
-    path('login/form/', views.login_form, name='login_form'),  # Ensure this line exists
-    path('signup/form/', views.signup_form, name='signup_form'),  # Ensure this line exists
+    path('login/form/', views.login_form, name='login_form'), 
+    path('signup/form/', views.signup_form, name='signup_form'),
 ]
