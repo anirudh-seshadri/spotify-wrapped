1c831caf0516c4bb3a740215a64f606e2dc8f2fc
Dhruv Sureka <dhruvsureka308@gmail.com>
2024-11-14 17:22:49 -0500

    Implemented a lot of changes to accomodate dark and light mode onto the frontend pages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  darkMode: 'class',
   content: [
-    './templates/**/*.html',       // Django template path for HTML files
+    './web_app/templates/**/*.html',       // Django template path for HTML files
     './web_app/static/**/*.js',    // Updated path for JS files inside web_app/static
     './web_app/static/**/*.css',   // Updated path for CSS files inside web_app/static
   ],
